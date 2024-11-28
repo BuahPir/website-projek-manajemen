dca784702c8b673e59a43ae2b1f9137bebf1fa75
BuahPir <andinadil90@gmail.com>
2024-11-28 20:15:28 +0800

    New Content

--- resources/views/coba.blade.php
+++ resources/views/coba.blade.php
@@ -8,7 +8,7 @@
     <div id="myModal" class="hidden fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
         <div class="bg-white p-6 rounded-lg shadow-md w-96">
             <h2 class="text-lg font-bold mb-4">Modal Content</h2>
-            <p>This is the modal content.</p>
+            <p>This is the modal content. Right</p>
             <button id="closeModalButton" class="mt-4 px-4 py-2 bg-red-500 text-white rounded-md">
                 Close Modal
             </button>
